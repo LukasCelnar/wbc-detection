024d3fa960169b45367cb90e54795d09aef53948
Drizz159 <celnar.lukas@gmail.com>
2021-10-17 12:35:14 +0200

    Added manual_trade in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -8,29 +8,27 @@ try:
 
     print(f'\nProcessing from {input_folder_name} folder\n')
     mkdir(output_folder_name)
+    mkdir(path.join(output_folder_name, 'data'))
+    mkdir(path.join(output_folder_name, 'manual_test'))
 
     images_path = f'{input_folder_name}\dataset2-master\dataset2-master\images'
 
     labels = ['EOSINOPHIL', 'LYMPHOCYTE', 'MONOCYTE', 'NEUTROPHIL']
 
-    """
-    EOSINOPHIL = 0
-    LYMPHOCYTE = 1
-    MONOCYTE = 2
-    NEUTROPHIL = 3
-    """
-
     for i, label in enumerate(labels):
         train_label_path = path.join(images_path, 'TRAIN', label)
         test_label_path = path.join(images_path, 'TEST', label)
+        manual_test_label_path = path.join(images_path, 'TEST_SIMPLE', label)
 
         train_images = listdir(train_label_path)
         test_images = listdir(test_label_path)
+        manual_test_images = listdir(manual_test_label_path)
 
         print(f'Copying {len(train_images)} train images and {len(test_images)} test images for {label}')
 
-        copytree(train_label_path, path.join(f'{output_folder_name}', label), dirs_exist_ok=True)
-        copytree(test_label_path, path.join(f'{output_folder_name}', label), dirs_exist_ok=True)
+        copytree(train_label_path, path.join(f'{output_folder_name}', 'data', label), dirs_exist_ok=True)
+        copytree(test_label_path, path.join(f'{output_folder_name}', 'data', label), dirs_exist_ok=True)
+        copytree(manual_test_label_path, path.join(f'{output_folder_name}', 'manual_test', label), dirs_exist_ok=True)
 
     success_message = f'Successfuly processed data into {output_folder_name} folder'
     print(f'\n{"-" * len(success_message)}\n{success_message}\n{"-" * len(success_message)}')
